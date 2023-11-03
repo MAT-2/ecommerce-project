dbdfcabb322549d5c6843d4a2f3cb58e55e2c037
MAT-2 <mpcasey30@gmail.com>
2023-11-03 10:02:28 -0500

    Had to fix syntax for destroy method in category-routes.js, since destroy was spelled wrong. Added delete HTTP request in tag-routes.js file.

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -45,8 +45,24 @@ router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res
+        .status(400)
+        .json({ message: "There is no tag that exist with that ID inputted." });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
