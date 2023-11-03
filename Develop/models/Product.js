232d4ddb09a629065c7fdcdf7a9e7ebd25b5b76f
MAT-2 <mpcasey30@gmail.com>
2023-11-02 22:04:38 -0500

    Noticed that executing node server.js is not working properly, most likely due to how the files are in the Developer folder and not in root. Will need to either put things to root or add Developer to the export statements. Added post request for category.

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,8 +1,8 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require("../../config/connection");
-
+const sequelize = require("../../Develop/config/connection");
+// ./../config/connection
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -32,8 +32,16 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_id: req.body.category_id,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(404).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
