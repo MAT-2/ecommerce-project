226a3cea0fa39a38bf87d18ce1e10f20d33203f5
MAT-2 <mpcasey30@gmail.com>
2023-11-03 19:01:15 -0500

    Added PUT HTTP request method for Tag-Routes js file.

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -41,8 +41,16 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.put({
+      tag_id: req.body.tag_id,
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(404).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
